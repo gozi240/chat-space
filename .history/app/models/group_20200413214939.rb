d43e14e50a852d4ceeeefc478835e913fe20813d
gozi240 <gozi240@gmail.com>
2020-04-13 21:57:46 +0900

    メッセージ送信機能実装

--- .history/app/models/group_20200413214939.rb
+++ .history/app/models/group_20200413214939.rb
@@ -6,15 +6,15 @@ class Group < ApplicationRecord
   validates :name, presence: true, uniqueness: true
 
   def show_last_message
-    if (last_message = messages.last).present?
+    if (last_message = message.last)
       if last_message.content?
-        last_message.content
+        last_message.content?
       else
-        '画像が投稿されています'
+        '画像が投稿sれています'
       end
     else
-      'まだメッセージはありません。'
+      'まだメッセージはありません'
     end
   end
-
+  
 end
--- .history/app/models/group_20200413215116.rb
+++ .history/app/models/group_20200413215116.rb
@@ -7,13 +7,14 @@ class Group < ApplicationRecord
 
   def show_last_message
     if (last_message = messages.last).present?
+    # if (last_message = messages.last).present?
       if last_message.content?
-        last_message.content
+        last_message.content?
       else
-        '画像が投稿されています'
+        '画像が投稿sれています'
       end
     else
-      'まだメッセージはありません。'
+      'まだメッセージはありません'
     end
   end
 
