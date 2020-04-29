4de125c4e7896b7508ba2a7a2df61cc557645d4b
gozi240 <gozi240@gmail.com>
2020-04-29 21:49:56 +0900

    チャット部分修正

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,43 +1,47 @@
 $(function(){ 
   function buildHTML(message){
+    console.log(message)
     if ( message.image ) {
         //data-idが反映されるようにしている
         var html =
-         `<div class="message" data-message-id=${message.id}>
-            <div class="upper-message">
-              <div class="upper-message__user-name">
-                ${message.user_name}
-              </div>
-              <div class="upper-message__date">
-                ${message.created_at}
-              </div>
-            </div>
-            <div class="lower-message">
-              <p class="lower-message__content">
-                ${message.content}
-              </p>
-            </div>
-            <img src=${message.image} >
-          </div>`
+                 `<div class="message" data-message-id="${message.id}">
+                  <div class="upper-message">
+                  <div
+                  ${message.user_name}
+                  </div>
+                  <div class="upper-message__info__date">
+                  ${message.created_at}
+                  </div>
+                  </div>
+                  <div class="lower-message">
+                  <p class="lower-message__content">
+                  ${message.content}
+                  </p>
+                  <img class="lower-message__image" src="${message.image}">
+                  </div>
+                  </div>
+                  </div>`
         return html;
     } else {
         //同様にdata-idが反映されるようにしている
         var html =
-         `<div class="message" data-message-id=${message.id}>
-            <div class="upper-message">
-              <div class="upper-message__user-name">
-                ${message.user_name}
-              </div>
-              <div class="upper-message__date">
-                ${message.created_at}
-              </div>
-            </div>
-            <div class="lower-message">
-              <p class="lower-message__content">
-                ${message.content}
-              </p>
-            </div>
-          </div>`
+                  `<div class="message" data-message-id="${message.id}">
+                    <div class="upper-message">
+                    <div class="upper-message__info">
+                    <div class="upper-message__info__user-name">
+                    ${message.user_name}
+                    </div>
+                    <div class="upper-message__info__date">
+                    ${message.created_at}
+                    </div>
+                    </div>
+                    <divlass="lower-message">
+                    <p class="lower-message__content">
+                    ${message.content}
+                    </p>
+                    </div>
+                    </div>
+                  </div>`
         return html;
     };
   }
@@ -45,7 +49,9 @@ $(function(){
   $('#new_message').on('submit', function(e){
     e.preventDefault();
     var formData = new FormData(this);
+    console.log(formData)
     var url = $(this).attr('action');
+    console.log(url)
     $.ajax({
       url: url,
       type: "POST",
@@ -55,7 +61,9 @@ $(function(){
       contentType: false
     })
     .done(function(data){
+      // console.log(data)
       var html = buildHTML(data);
+      // console.log(html)
       $('.messages').append(html);
       $('#new_message')[0].reset();
       var height = $('.messages')[0].scrollHeight;
