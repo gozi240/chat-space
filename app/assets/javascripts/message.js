4473e4daf526b9adf9727227733ac063630fca5f
gozi240 <gozi240@gmail.com>
2020-04-30 19:53:31 +0900

    chat-space 最終調整

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -2,46 +2,41 @@ $(function(){
   function buildHTML(message){
     console.log(message)
     if ( message.image ) {
-        //data-idが反映されるようにしている
         var html =
-                 `<div class="message" data-message-id="${message.id}">
-                  <div class="upper-message">
-                  <div
+        `<div class="message" data-message-id="${message.id}">
+            <div class="upper-message">
+                <div class="upper-message__user-name">
                   ${message.user_name}
-                  </div>
-                  <div class="upper-message__info__date">
+                </div>
+                <div class="upper-message__date">
                   ${message.created_at}
-                  </div>
-                  </div>
-                  <div class="lower-message">
-                  <p class="lower-message__content">
-                  ${message.content}
-                  </p>
-                  <img class="lower-message__image" src="${message.image}">
-                  </div>
-                  </div>
-                  </div>`
+                </div>
+            <div class="lower-message">
+              <p class="lower-message__content">
+                ${message.content}
+              </p>
+            <class="lower-message__image>
+              <img src="${message.image}>
+            </div>  
+          </div>`
         return html;
     } else {
-        //同様にdata-idが反映されるようにしている
         var html =
-                  `<div class="message" data-message-id="${message.id}">
-                    <div class="upper-message">
-                    <div class="upper-message__info">
-                    <div class="upper-message__info__user-name">
-                    ${message.user_name}
-                    </div>
-                    <div class="upper-message__info__date">
-                    ${message.created_at}
-                    </div>
-                    </div>
-                    <divlass="lower-message">
-                    <p class="lower-message__content">
-                    ${message.content}
-                    </p>
-                    </div>
-                    </div>
-                  </div>`
+          `<div class="message" data-message-id="${message.id}">
+            <div class="upper-message">
+              <div class="upper-message__user-name">
+                ${message.user_name}
+              </div>
+              <div class="upper-message__date">
+                ${message.created_at}
+              </div>
+            </div>
+            <divlass="lower-message">
+              <p class="lower-message__content">
+                ${message.content}
+              </p>
+            </div>
+          </div>`
         return html;
     };
   }
