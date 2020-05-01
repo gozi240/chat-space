656eff9e052d899b3b4197a604d942ae5f64f92f
gozi240 <gozi240@gmail.com>
2020-05-01 20:06:04 +0900

    deploy後修正

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -2,42 +2,42 @@ $(function(){
   function buildHTML(message){
     console.log(message)
     if ( message.image ) {
-        var html =
-        `<div class="message" data-message-id="${message.id}">
-            <div class="upper-message">
-                <div class="upper-message__user-name">
-                  ${message.user_name}
-                </div>
-                <div class="upper-message__date">
-                  ${message.created_at}
-                </div>
+      var html =
+        `<div class="message" data-message-id=${message.id}>
+          <div class="upper-message">
+            <div class="upper-message__user-name">
+              ${message.user_name}
+            </div>
+            <div class="upper-message__date">
+              ${message.created_at}
+            </div>
+          </div>
+          <div class="lower-message">
+            <p class="lower-message__content">
+            ${message.content}
+            </p>
+          </div>
+          <img class="lower-message__image" src="${message.image}">
+        </div>`
+      return html;
+    } else {
+      var html =
+        `<div class="message" data-message-id=${message.id}>
+          <div class="upper-message">
+            <div class="upper-message__user-name">
+              ${message.user_name}
+            </div>
+            <div class="upper-message__date">
+              ${message.created_at}
+            </div>
+          </div>
             <div class="lower-message">
               <p class="lower-message__content">
                 ${message.content}
               </p>
-            <class="lower-message__image>
-              <img src="${message.image}>
-            </div>  
-          </div>`
-        return html;
-    } else {
-        var html =
-          `<div class="message" data-message-id="${message.id}">
-            <div class="upper-message">
-              <div class="upper-message__user-name">
-                ${message.user_name}
-              </div>
-              <div class="upper-message__date">
-                ${message.created_at}
-              </div>
             </div>
-            <divlass="lower-message">
-              <p class="lower-message__content">
-                ${message.content}
-              </p>
-            </div>
-          </div>`
-        return html;
+         </div>`
+      return html;
     };
   }
  
@@ -56,9 +56,9 @@ $(function(){
       contentType: false
     })
     .done(function(data){
-      // console.log(data)
+      console.log(data)
       var html = buildHTML(data);
-      // console.log(html)
+      console.log(html)
       $('.messages').append(html);
       $('#new_message')[0].reset();
       var height = $('.messages')[0].scrollHeight;
