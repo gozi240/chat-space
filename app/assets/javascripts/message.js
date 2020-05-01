c82a9ff8b49f34a6bdca7c5f0fd94725b28fd7e3
gozi240 <gozi240@gmail.com>
2020-05-01 20:52:03 +0900

    deploy後修正

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,45 +1,46 @@
 $(function(){ 
   function buildHTML(message){
-    console.log(message)
     if ( message.image ) {
-      var html =
-        `<div class="message" data-message-id=${message.id}>
-          <div class="upper-message">
-            <div class="upper-message__user-name">
-              ${message.user_name}
+        //data-idが反映されるようにしている
+        var html =
+         `<div class="message" data-message-id=${message.id}>
+            <div class="upper-message">
+              <div class="upper-message__user-name">
+                ${message.user_name}
+              </div>
+              <div class="upper-message__date">
+                ${message.created_at}
+              </div>
             </div>
-            <div class="upper-message__date">
-              ${message.created_at}
-            </div>
-          </div>
-          <div class="lower-message">
-            <p class="lower-message__content">
-            ${message.content}
-            </p>
-          </div>
-          <img class="lower-message__image" src="${message.image}">
-        </div>`
-      return html;
-    } else {
-      var html =
-        `<div class="message" data-message-id=${message.id}>
-          <div class="upper-message">
-            <div class="upper-message__user-name">
-              ${message.user_name}
-            </div>
-            <div class="upper-message__date">
-              ${message.created_at}
-            </div>
-          </div>
             <div class="lower-message">
               <p class="lower-message__content">
                 ${message.content}
               </p>
             </div>
-         </div>`
-      return html;
-    };
-  }
+            <img src=${message.image} >
+          </div>`
+        return html;
+      } else {
+        //同様にdata-idが反映されるようにしている
+        var html =
+         `<div class="message" data-message-id=${message.id}>
+            <div class="upper-message">
+              <div class="upper-message__user-name">
+                ${message.user_name}
+              </div>
+              <div class="upper-message__date">
+                ${message.created_at}
+              </div>
+            </div>
+            <div class="lower-message">
+              <p class="lower-message__content">
+                ${message.content}
+              </p>
+            </div>
+          </div>`
+        return html;
+      };
+    }
  
   $('#new_message').on('submit', function(e){
     e.preventDefault();
